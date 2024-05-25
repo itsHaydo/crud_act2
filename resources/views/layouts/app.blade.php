8cf5813f0067134d85072c543af0e11217d7215a
itsHaydo <2130199@upv.edu.mx>
2024-05-25 11:55:36 -0600

    Crud

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UPV</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+<body>   
+
+    <div class="container">
+        <h3 class=" mt-3">TAI - UPV</a></h3>
+            @yield('content')
+            <div class="row justify-content-center text-center mt-3">
+                <div class="col-md-12">
+                    <p>
+                        UPV 2024
+                        
+                    </p>
+                    <p>
+                        Derechos reservados2024 
+                    </p>
+                </div>
+            </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
